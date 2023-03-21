aa04e3fae521a192c935b798ac20c7b22375edfa
Sam Young <107888265+SamY724@users.noreply.github.com>
2023-03-21 17:39:27 +0000

    Updates

---
+++ precache-manifest.93af6955b47457cd5b77c5fddd1ac07e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaee72994868fcf47e8b",
+    "url": "/WebsiteSY/static/css/main.1198d1f4.chunk.css"
+  },
+  {
+    "revision": "aaee72994868fcf47e8b",
+    "url": "/WebsiteSY/static/js/main.aaee7299.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/WebsiteSY/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "d58c719e4f382562ed34",
+    "url": "/WebsiteSY/static/js/2.d58c719e.chunk.js"
+  },
+  {
+    "revision": "f2266e4dc32a21cb704a",
+    "url": "/WebsiteSY/static/js/runtime~main.f2266e4d.js"
+  },
+  {
+    "revision": "8e61caf4e2b9115633a9f20173d6e6dc",
+    "url": "/WebsiteSY/static/media/logo1.8e61caf4.png"
+  },
+  {
+    "revision": "da681497ff24a625bc4711875d08b041",
+    "url": "/WebsiteSY/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/websiteSY/precache-manifest.62202a5ea031b25f022028d1a33dc761.js"
+  "/WebsiteSY/precache-manifest.93af6955b47457cd5b77c5fddd1ac07e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/websiteSY/index.html", {
+workbox.routing.registerNavigationRoute("/WebsiteSY/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
